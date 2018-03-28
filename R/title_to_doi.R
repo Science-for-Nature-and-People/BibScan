f6e54727c0222d9d26cb749e2c7d0cf1d6333c6b
Stephen Wood <stephenawood@gmail.com>
2018-03-28 14:11:54 -0400

    Removed separate .bib read process

--- R/title_to_doi.R
+++ R/title_to_doi.R
@@ -2,30 +2,27 @@
 #'
 #' This function matches titles exported from Colandr to .bib files
 #' and exports the associated DOI from Web of Science
-#' @param papers A .csv exported from Colandr
-#' @param bib.dir A directory containing all .bib files imported to Colandr
+#' @param papers    A .csv exported from Colandr
+#' @param file_list A tibble of .bib files imported to Colandr
+#' @param cond      Condition for sorting papers
 #' @keywords Colandr
 #' @return A data frame of titles, journals, authors, and DOIs in the .bib format
 #' @export
 #' @examples
 #' title_to_doi()
 
-title_to_doi <- function(papers,bib.dir){
+title_to_doi <- function(papers,bib.dir,condition){
   require(bibliometrix)
   require(tidyverse)
 
   # filter list of papers by those that are included
   # select only the relevant columns
-  papers = filter(papers, citation_screening_status == "included") %>%
+  papers = filter(papers, citation_screening_status == condition) %>%
     select(citation_title,citation_authors,citation_journal_name)
 
   # read in bib files
   # select only relevant columns
-  references = as.list(file.path(bib.dir, dir(bib.dir))) %>%
-    do.call(readFiles,.) %>%
-    convert2df() %>%
-    as_tibble() %>%
-    select(TI,AU,PY,JI,SO,DI)
+  references = file_list
 
   # convert title text to lower case
   papers$citation_title <- tolower(papers$citation_title)
